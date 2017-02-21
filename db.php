84075828661e159c110cbe3c9f76d00d748eb1b5
Ahamed A <arhammail@gmail.com>
2017-02-21 13:32:43 +0000

    Established DB connection to Azure DB

--- db.php
+++ db.php
@@ -1,68 +1,64 @@
 <?php
+/*
+ * Represents a connection to the database.
+ */
+class db {
 
   /*
-   * Represents a connection to the database.
+   * Property containing the MySQLi connection object.
+   * This is 'static' to save resources - the same connection will be used across multiple instances.
    */
-  class db
-  {
+  private static $mysqli;
 
-    /*
-     * Property containing the MySQLi connection object.
-     * This is 'static' to save resources - the same connection will be used across multiple instances.
-     */
-    private static $mysqli;
-
-    /*
-     * Opens a new connection to the database.
-     * Returns: nothing.
-     */
-    public function connect() {
-      // Only set up the connection the first time this is called.
-      if (!isset(self::$mysqli)) {
-        // Details to use for connection
-        $host = "eu-cdbr-azure-west-d.cloudapp.net";
-        $username = "b572eae4b8201c";
-        $password = "e865fdac";
-        $dbname = "social_media";
+  /*
+   * Opens a new connection to the database.
+   * Returns: nothing.
+   */
+  public function connect() {
+    // Only set up the connection the first time this is called.
+    if (!isset(self::$mysqli)) {
+      // Details to use for connection
+      $host = "eu-cdbr-azure-west-d.cloudapp.net";
+      $username = "b572eae4b8201c";
+      $password = "e865fdac";
+      $dbname = "social_media";
 
 
-        // Create connection object and assign it to property
-        self::$mysqli = new mysqli($host, $username, $password, $dbname);
-      }
-
-      // Check for errors
-      if (!self::$mysqli) {
-        // NOTE: This will kill the entire page!
-        die("Database connection has failed: " . self::$mysqli->connect_error . " (" . self::$mysqli->connect_errno . ")");
-      }
+      // Create connection object and assign it to property
+      self::$mysqli = new mysqli($host, $username, $password, $dbname);
     }
 
-    /*
-     * Performs a query on the database.
-     * $query: the query as a string.
-     * Returns false on failure. For successful queries, returns a mysqli_result object or true depending on if the query requires data to be returned.
-     */
-    public function query($query) {
-      return self::$mysqli->query($query);
-    }
-
-    /*
-     * Prepares a SQL statement.
-     * $query: the query as a string.
-     * Returns false on failure. For successful queries, returns a mysqli_stmt object.
-     */
-    public function prepare($query) {
-      return self::$mysqli->prepare($query);
-    }
-
-    /*
-     * Returns the last mysqli error message.
-     */
-    public function getError() {
-      return self::$mysqli->error;
-    }
-
-    public function closeConnection() {
-        self::$mysqli->close();
+    // Check for errors
+    if (!self::$mysqli) {
+      // NOTE: This will kill the entire page!
+      die("Database connection has failed: " . self::$mysqli->connect_error . " (" . self::$mysqli->connect_errno . ")");
     }
   }
+
+  /*
+   * Performs a query on the database.
+   * $query: the query as a string.
+   * Returns false on failure. For successful queries, returns a mysqli_result object or true depending on if the query requires data to be returned.
+   */
+  public function query($query) {
+    return self::$mysqli->query($query);
+  }
+
+  /*
+   * Prepares a SQL statement.
+   * $query: the query as a string.
+   * Returns false on failure. For successful queries, returns a mysqli_stmt object.
+   */
+  public function prepare($query) {
+    return self::$mysqli->prepare($query);
+  }
+
+  /*
+   * Returns the last mysqli error message.
+   */
+  public function getError() {
+    return self::$mysqli->error;
+  }
+}
+
+?>
