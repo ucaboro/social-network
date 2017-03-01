c3c748dd16997d3498165640c879a896988398fd
Anton Morozov <anton.morozov.16@ucl.ac.uk>
2017-03-01 15:35:33 +0000

    connection to azure db

--- db.php
+++ db.php
@@ -19,6 +19,10 @@ class db {
     // Only set up the connection the first time this is called.
     if (!isset(self::$mysqli)) {
       // Details to use for connection
+      $host = "eu-cdbr-azure-west-d.cloudapp.net";
+      $username = "b572eae4b8201c";
+      $password = "e865fdac";
+      $dbname = "social_media";
 
 
       // Create connection object and assign it to property
