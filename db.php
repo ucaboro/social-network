74e91768dbd35dc064136ef35423383ad7f4b075
Michael <michael.scott.16@ucl.ac.uk>
2017-03-08 21:50:06 +0000

    switch back to azure in db.php

--- db.php
+++ db.php
@@ -18,14 +18,10 @@ class db {
     // Only set up the connection the first time this is called.
     if (!isset(self::$mysqli)) {
       // Details to use for connection
-      /*$host = "eu-cdbr-azure-west-d.cloudapp.net";
+      $host = "eu-cdbr-azure-west-d.cloudapp.net";
       $username = "b572eae4b8201c";
       $password = "e865fdac";
-      $dbname = "social_media";*/
-      $host = "Localhost";
-      $username = "michaelOld";
-      $password = "aQRpuC5GqyTFCHwD";
-      $dbname = "newNetworking";
+      $dbname = "social_media";
 
 
       // Create connection object and assign it to property
