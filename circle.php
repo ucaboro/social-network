76d1033c27096e19b02dd2c40c7bf177cbeb1cf3
Anton Morozov <anton.morozov.16@ucl.ac.uk>
2017-03-15 02:15:38 +0000

    Fixing NewCircle creation bug + add Users

--- circle.php
+++ circle.php
@@ -85,21 +85,120 @@ checkLoggedIn(); ?>
             </div>
           </div>
           <div class="row">
-            <div class="col-xs-12">
-
+            <div id="users" class="col-xs-12">
               <?php
-              echo getHtmlForJavascriptImports();
               echo getHtmlForCircleUsersPanel($circle);
-              echo getHtmlForCirclePanel();
-              echo getHtmlForNewCircle();
               echo messagingScript();
               ?>
+              <div class="row">
+                <div id="outer"  class="col-xs-12">
+                  <?php  echo getHtmlForCirclePanel();?>
+                </div>
 
             </div>
+
           </div>
         </div>
       </div>
       </div>
+<?php   echo getHtmlForNewCircle() ;?>
 
+
+<div class="modal fade" id="addUsers" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+  <div class="modal-dialog" role="document">
+      <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="addTitle">Add users to the circle</h4>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="control-label">Friends:</label>
+
+            <div class="panel-body">
+              <?php
+                //send array variables to ajax
+                //header("Content-type: text/javascript");
+
+                $friends = getUser()->getFriends();
+                $allUserIds = array();
+              // Output each one
+              foreach ($friends as $friend) {
+                echo getHtmlForAddUserResult($friend, true, false, false);
+                  array_push($allUserIds,  $friend->id);
+
+              }
+
+                $allUserIds_json = json_encode($allUserIds);
+
+              ?>
+            </div>
+
+          </div>
+
+      <div class="modal-footer">
+        <button id="close" type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>";
   </body>
 </html>
+<script type='text/javascript'>
+//ADD NEW USER
+
+
+//retrieving array from php
+var circle = $('#circleID').val();
+var obj = JSON.parse('<?= $allUserIds_json; ?>');
+console.log(("There are " + obj));
+
+//recieve array from php
+function tick(){
+  var id = this.id;
+  var sign = document.getElementById(id).className;
+  document.getElementById(id).className ="glyphicon glyphicon-ok";
+
+
+    //send to the db
+    var xhr = new XMLHttpRequest();
+   xhr.open('POST','ajax/addUsers.php', true);
+   xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+   xhr.onreadystatechange = function (){
+
+     if (xhr.readyState == 4 && xhr.status ==200){
+          var target = document.getElementById("users");
+                 target.innerHTML = xhr.responseText;
+     }
+  }
+  xhr.send("id="+id+"&circle="+circle);
+  }
+
+
+
+//on close change all back to plus
+function restoreSign(){
+
+  for (i = 0; i < obj.length; i++) {
+  document.getElementById(obj[i]).className="glyphicon glyphicon-plus";
+  }
+}
+
+//create close button
+document.getElementById("close").onclick = restoreSign;
+
+//create add button
+document.getElementById("add").onclick = addUsers;
+
+
+//create add button
+for (i = 0; i < obj.length; i++) {
+  var id = document.getElementById(obj[i]);
+  id.addEventListener("click", tick);
+}
+
+
+
+
+</script>
